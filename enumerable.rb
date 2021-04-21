0f4edac57ac369fbcef5a9bf184a521dfe55827a
Moises <mhdezcoronado@gmail.com>
2021-04-21 14:59:42 +0200

    Add my_select to work with hashes

--- enumerable.rb
+++ enumerable.rb
@@ -13,13 +13,27 @@ module Enumerable
 
   def my_select
     new_arr = []
-    for i in self
-        new_arr << i if yield(i) == true
+    new_hash = {}
+    self.my_each do |k,v|
+        if self.class == Array
+            new_arr << k if yield(k) == true
+        else
+            new_hash.store(k,v) if yield(k,v) == true
+        end
     end
-    new_arr
+    new_arr.empty? ? new_hash : new_arr
   end
 end
 
 p [2,4,3,5,43].my_select {|v| v.even? }
 
-# {"three"=>"one", "four"=> "two", "one"=> "three"}.my_each_with_index { |(k,v), i | puts "#{k} : #{v} has index #{i}"}
+stock = {
+    apples: '10',
+    oranges: 'key-value',
+    bananas: 1
+}
+
+p stock.my_select { |k,v| v == 'key-value' }
+
+
+# h = {} h.merge!(key: "bar") # => {:key=>"bar"} 
