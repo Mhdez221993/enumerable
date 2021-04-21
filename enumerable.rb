7f487ad6322947a1b75152d82730ccfcbf360cb5
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-04-21 12:47:29 +0200

    Add my_each_with_index hashes functionality

--- enumerable.rb
+++ enumerable.rb
@@ -7,7 +7,7 @@ module Enumerable
 
   def my_each_with_index
     for i in self do
-        yield(i, self.index(i))
+      self.class == Array ? yield(i, self.index(i)) : yield([i[0], i[1]], self.keys.index(i[0]))
     end
   end
 
@@ -17,3 +17,7 @@ module Enumerable
     end
   end
 end
+
+[2,4,3,5,43].my_each_with_index {| v, i| puts "#{v} has index #{i}"}
+
+{"three"=>"one", "four"=> "two", "one"=> "three"}.my_each_with_index { |(k,v), i | puts "#{k} : #{v} has index #{i}"}
