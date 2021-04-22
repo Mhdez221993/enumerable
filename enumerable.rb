ecec814e5915a9b7dbe2fe722e134aa36e3f9420
Moises <mhdezcoronado@gmail.com>
2021-04-22 11:36:36 +0200

    Make my_any to when block given

--- enumerable.rb
+++ enumerable.rb
@@ -54,48 +54,57 @@ module Enumerable
   def my_any?(*args)
     if block_given? == false
       if args.empty?
-        return !self.empty?
+        return !self.empty? ? self.my_each {|v| return true if v != nil} == false : false
       else
         return my_each { |v| return true if v.is_a? *args } == false if args[0].class == Class
         return my_each { |v| return true if v.match(args[0]) } == false if args[0].class == Regexp
         return my_each { |v| return true if v == args[0] } == false
       end
     else
-      p "block given"
+      my_each { |v| return true if yield(v) } == false
     end
   end
 end
 
 
-# p [1,2,2].my_any?
-# p [1, 5, 3.14].all?(5)
+# p [1,2,2].my_any?(5)
+# p [1,2,2].any?(5)
+# p '-----------------------------------'
+# p [nil, nil].my_any?
+# p [nil, nil].any?
+# p '-----------------------------------'
 
-# p [nil, nil, nil].my_all?
-# p [nil, nil, nil].all?
+# p ["some", "somithimes", "something"].my_any?(/s/)
+# p ["some", "somithimes", "something"].any?(/s/)
 
-# p ["some", "somithimes", "something"].my_all?(/s/)
-# p ["some", "somithimes", "something"].all?(/s/)
-
-# p %w[ant bear cat].my_all?(/t/)  
-# p %w[ant bear cat].all?(/t/)  
+# p '-----------------------------------'
+# p %w[ant bear cat].my_any?(/t/)  
+# p %w[ant bear cat].any?(/t/)  
+# p '-----------------------------------'
 
 # p [2,3,3].my_any?(3) 
 # p [1,2,3].any?(3) 
+# p '-----------------------------------'
 
 # p [1, 2i, 3.14, 5, 's'].my_any?(Numeric) 
 # p [1, 2i, 3.14].any?(Numeric) 
+# p '-----------------------------------'
 
-# p %w[ant bear cat].my_all? { |word| word.length >= 3 }
-# p %w[ant bear cat].all? { |word| word.length >= 3 }
+# p %w[ant bear cat].my_any? { |word| word.length >= 10 }
+# p %w[ant bear cat].any? { |word| word.length >= 10 }
+# p '-----------------------------------'
 
-# p %w[ant bear cat].my_all? { |word| word.length >= 4 }
-# p %w[ant bear cat].all? { |word| word.length >= 4 }
+# p %w[ant bear cat].my_any? { |word| word.length >= 4 }
+# p %w[ant bear cat].any? { |word| word.length >= 4 }
+# p '-----------------------------------'
 
-# p %w[ant bear cat].my_all? 
-# p %w[ant bear cat].all? 
+# p %w[ant bear cat].my_any? 
+# p %w[ant bear cat].any? 
+# p '-----------------------------------'
 
-# p [ nil, true, 99].my_all?  
-# p [nil, true, 99].all?  
+# p [ nil, true, 99].my_any?  
+# p [nil, true, 99].any?  
+# p '-----------------------------------'
 
 # p [1,2,3].my_any?
 # p [1,2,3].any?
