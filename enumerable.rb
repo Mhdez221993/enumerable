cc3fec7ea0bbb034ccc43e2223dde335d28b172f
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-04-23 14:56:31 +0200

    Add 'localJumpError' to my_inject method

--- enumerable.rb
+++ enumerable.rb
@@ -120,6 +120,7 @@ module Enumerable
 
   def my_inject(*args)
     if block_given? == false
+      raise LocalJumpError, 'no block given' if args.empty?
       accu = args.size < 2 ? to_a[0] : args[0]
       to_a.my_each_with_index do |_v, i|
         accu = accu.send(args[0], to_a[i + 1]) if args.size < (2) && !to_a[i + 1].nil?
