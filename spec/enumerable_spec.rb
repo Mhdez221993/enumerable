f7bc660b5d755ff74ecac48c0022957a065375b5
Safa <safaaballagh97@gmail.com>
2021-05-04 08:41:00 +0000

    Add test for my_each_with_index

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -11,5 +11,27 @@ describe Enumerable do
        it "return the same array" do
             expect(arr.my_each {|v| v+1}).to eq arr
        end
+
+       it "Iterate over the array and update a new one" do
+        new_arr = []
+        arr.my_each{|v| new_arr << v+1}
+        expect(new_arr).to eq([2,3,4,5]) 
+   end
     end
+
+    describe "#my_each_with_index" do
+        it "return the enumerable" do
+            expect(arr.my_each_with_index).to be_a(Enumerator)
+        end
+ 
+        it "return the same array" do
+             expect(arr.my_each_with_index {|v, i| v+1}).to eq arr
+        end
+
+        it "Iterate over the array and update a new one" do
+            new_arr = []
+            arr.my_each_with_index {|v, i| new_arr << v+1}
+            expect(new_arr).to eq([2,3,4,5]) 
+       end
+     end
 end
